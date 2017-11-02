ca42d6ac89a810e55241a35ba7d3dbbe883cda40
Shota Nagahori <s-nagahori@unext.jp>
2017-11-02 17:52:11 +0900

    Add new assertion (red)

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -9,6 +9,8 @@ class MoneyTest {
         val five = Dollar(5)
         five.times(2)
         assertEquals(10, five.amount)
+        five.times(3)
+        assertEquals(15, five.amount)
     }
 }
 
