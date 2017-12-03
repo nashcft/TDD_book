42e07e5e8055713c2f6e01e01436606a9d106450
nashcft <nashrfv@gmail.com>
2017-12-03 13:07:25 +0900

    Remove the inheritance of currency

--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -1,8 +1,6 @@
 package money
 
-sealed class Money(private val amount: Int) {
-
-    abstract val currency: String
+sealed class Money(private val amount: Int, val currency: String) {
 
     fun times(mul: Int): Money {
         return when(this) {
@@ -12,10 +10,10 @@ sealed class Money(private val amount: Int) {
     }
 
     companion object {
-        fun dollar(amount: Int): Money = Money.Dollar(amount)
-        fun franc(amount: Int): Money = Money.Franc(amount)
+        fun dollar(amount: Int): Money = Money.Dollar(amount, "USD")
+        fun franc(amount: Int): Money = Money.Franc(amount, "CHF")
     }
 
-    data class Dollar(private val a: Int, override val currency: String = "USD") : Money(a)
-    data class Franc(private val a: Int, override val currency: String = "CHF") : Money(a)
+    data class Dollar(private val a: Int, private val c: String) : Money(a, c)
+    data class Franc(private val a: Int, private val c: String) : Money(a, c)
 }
