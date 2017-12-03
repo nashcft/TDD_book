d7fc3ca948444ab01675bc90b7e1e2727e8e90ab
nashcft <nashrfv@gmail.com>
2017-12-03 15:06:57 +0900

    Format code

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -9,20 +9,23 @@ import kotlin.test.assertTrue
 
 class MoneyTest {
 
-    @Test fun testMultiplication() {
+    @Test
+    fun testMultiplication() {
         val five = dollar(5)
         assertEquals(dollar(10), five.times(2))
         assertEquals(dollar(15), five.times(3))
     }
 
-    @Test fun testEquality() {
+    @Test
+    fun testEquality() {
         assertTrue(dollar(5) == dollar(5))
         assertFalse(dollar(5) == dollar(6))
 
         assertFalse(dollar(5) == franc(5))
     }
 
-    @Test fun testCurrency() {
+    @Test
+    fun testCurrency() {
         assertEquals("USD", dollar(1).currency)
         assertEquals("CHF", franc(1).currency)
     }
