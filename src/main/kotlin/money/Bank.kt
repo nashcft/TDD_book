d31c3204e548ef946cd1f8b6c64817b0447efd23
nashcft <nashrfv@gmail.com>
2017-12-03 21:23:17 +0900

    Refactor

--- src/main/kotlin/money/Bank.kt
+++ src/main/kotlin/money/Bank.kt
@@ -5,6 +5,5 @@ fun reduce(source: Expression, to: String) = when (source) {
     is Expression.Sum -> Money(sum(source.augend, source.addend, to), to)
 }
 
-private fun sum(augend: Expression, addend: Expression, to: String): Int {
-    return reduce(augend, to).amount + reduce(addend, to).amount
-}
+private fun sum(augend: Expression, addend: Expression, to: String): Int =
+        reduce(augend, to).amount + reduce(addend, to).amount
